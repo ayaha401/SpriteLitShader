c9ce811f88ab891d71a29f6b86d83c284c3ac65d
ayaha401 <oshizawadai.1192@gmail.com>
2021-10-25 13:49:09 +0900

    add SpriteLitShaderGUI

---
+++ Editor/SpriteLitShaderGUI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SpriteLitShaderGUI : ShaderGUI
+{
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] Prop)
+    {
+        var material = (Material)materialEditor.target;
+
+        
+
+        GUILayout.Label("Information", EditorStyles.boldLabel);
+        using (new EditorGUILayout.VerticalScope("box"))
+        {
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Label("Version");
+                GUILayout.FlexibleSpace();
+                GUILayout.Label("Version 1.0.0");
+            }
+
+            using (new EditorGUILayout.HorizontalScope())
+            {
+                GUILayout.Label("How to use (Japanese)");
+                GUILayout.FlexibleSpace();
+                if(GUILayout.Button("How to use (Japanese)"))
+                {
+                    System.Diagnostics.Process.Start("https://github.com/ayaha401/SpriteLitShader/wiki");
+                }
+            }
+        }
+
+        // ================================================================================================
+        GUILayout.Box("", GUILayout.Height(2), GUILayout.ExpandWidth(true));
+        // ================================================================================================
+
+        base.OnGUI (materialEditor, Prop);
+    }
+}
